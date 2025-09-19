8797702e65fedbf3c73d6649ee8183d737d4dda4
Kamil Wojkowski <wojkowski.kamil@gmail.com>
2025-09-19 15:01:06 +0200

    feat: basic form html structure

--- 05-reactive-forms/src/app/rick-and-morty/characters/create/create-character-container.ts
+++ 05-reactive-forms/src/app/rick-and-morty/characters/create/create-character-container.ts
@@ -1,13 +1,64 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CreateCharacterFormService } from './create-character.form.service';
 
+export enum CreateCharacterSteps {
+  BasicInfo = 1,
+  Location,
+  Episodes,
+  // Image,
+}
+
 @Component({
-  selector: 'app-create',
+  selector: 'app-create-character-container',
   imports: [],
   providers: [CreateCharacterFormService],
-  template: ` <p>create works!</p> `,
+  template: `
+    <progress [value]="currentStep()" [max]="CreateCharacterSteps.Episodes"></progress>
+
+    <section>
+      <h2>Step {{ currentStep() }} of {{ CreateCharacterSteps.Episodes }}</h2>
+      @switch (currentStep()) {
+        @case (CreateCharacterSteps.BasicInfo) {
+          <p>Basic Info Form</p>
+        }
+        @case (CreateCharacterSteps.Location) {
+          <p>Location Form</p>
+        }
+        @case (CreateCharacterSteps.Episodes) {
+          <p>Episodes Form</p>
+        }
+        <!-- @case (CreateCharacterSteps.Image) {
+          <p>Image Form</p>
+        } -->
+        @default {}
+      }
+    </section>
+
+    <footer style="display: flex; gap: 1rem">
+      <button (click)="back()" [disabled]="currentStep() === CreateCharacterSteps.BasicInfo">
+        Back
+      </button>
+      @if (currentStep() === CreateCharacterSteps.Episodes) {
+        <button>Submit</button>
+      } @else {
+        <button (click)="next()">Next</button>
+      }
+    </footer>
+  `,
   styles: ``,
 })
 export class CreateCharacterContainer {
   service = inject(CreateCharacterFormService);
+
+  CreateCharacterSteps = CreateCharacterSteps;
+
+  currentStep = signal(CreateCharacterSteps.BasicInfo);
+
+  back() {
+    this.currentStep.set(this.currentStep() - 1);
+  }
+
+  next() {
+    this.currentStep.set(this.currentStep() + 1);
+  }
 }
